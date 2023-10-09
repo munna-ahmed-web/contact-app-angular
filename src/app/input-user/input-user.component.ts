c48205bf3cf1d1e0cb3aecad000a5871e5bc003c
web.munnaahmed@gmail.com <web.munnaahmed@gmail.com>
2023-10-09 10:46:38 +0600

    third commit

--- src/app/input-user/input-user.component.ts
+++ src/app/input-user/input-user.component.ts
@@ -1,20 +1,30 @@
-import { Component, DoCheck, Input, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, inject } from '@angular/core';
 import { ContactDataService } from 'src/services/contact-data.service';
+import { UserDataShape } from 'src/types/type';
 
 @Component({
   selector: 'app-input-user',
   templateUrl: './input-user.component.html',
   styleUrls: ['./input-user.component.css'],
 })
-export class InputUserComponent {
+export class InputUserComponent implements OnChanges {
   userService = inject(ContactDataService);
   @Input('editUser') editUserData: any;
+  duplicateData: any = null;
 
   isEmptyObject(obj: Object) {
     return JSON.stringify(obj) === '{}';
   }
 
-  hasData : boolean = false;
+  isObjectReady = (obj: UserDataShape) => {
+    if (obj.phone && obj.email && obj.email) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  hasData: boolean = false;
   isConfirmed: boolean = true;
 
   initialObj = {
@@ -31,17 +41,30 @@ export class InputUserComponent {
   handleSubmit = () => {
     this.userService.setUserInfo(this.user);
     this.user = { ...this.initialObj };
+    if (this.hasData) {
+      this.hasData = false;
+    }
   };
 
   handleConfirm() {
     this.user = {
-      ...this.editUserData,
+      ...this.duplicateData,
     };
-    this.userService.deleteUser(this.editUserData);
-    this.isConfirmed = !this.isConfirmed;
+    this.userService.deleteUser(this.duplicateData);
+    // this.isConfirmed = !this.isConfirmed;
+    this.hasData = !this.hasData;
+
+    this.duplicateData = null
   }
 
   handleCancel() {
-    this.isConfirmed = !this.isConfirmed;
+    this.hasData = !this.hasData;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.editUserData) {
+      this.duplicateData = this.editUserData;
+      this.hasData = true;
+    }
   }
 }
